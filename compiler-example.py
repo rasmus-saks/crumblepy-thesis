01587be869b74753f6db413b751a79f7fd4f7135
Rasmus Saks <rasmussaks1@gmail.com>
2017-04-27 02:50:52 +0300

    Finish up final section, add conclusion and other required things. Touch up some places

--- compiler-example.py
+++ compiler-example.py
@@ -1,8 +1,8 @@
 class Compiler:
     def compile_stmt(self, node):
-        if type(node) == Module:  # Juurtipp
-            for n in node.body:
-                self.compile_stmt(n)  # Rekursioon
-        elif type(node) == If:
-            self.if_stmt(node)  # Kompileeri if-lause
+        if type(node) == Module:        # Juurtipp
+            for n in node.body:         # Läbi kõik juurtipu alamtipud
+                self.compile_stmt(n)    # Rekursioon
+        elif type(node) == If:          # if tipp
+            self.if_stmt(node)          # Kompileeri if-lause
         # ...
